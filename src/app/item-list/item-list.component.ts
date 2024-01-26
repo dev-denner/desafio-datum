2fa691af81734b595a5da5b52354628abe167fcc
Denner Fernandes <dev.denner@outlook.com>
2024-01-26 00:11:55 -0300

    feat: ✨ implement list and add data

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './item-list.component.sass'
 })
 export class ItemListComponent {
+  items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' }
+  ];
 
+  newItemName = '';
+
+  addItem() {
+    if (this.newItemName) {
+      const newItem = {
+        id: this.items.length + 1,
+        name: this.newItemName
+      };
+      this.items.push(newItem);
+      this.newItemName = '';
+    }
+  }
 }
